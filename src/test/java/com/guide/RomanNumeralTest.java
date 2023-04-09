8caafe38c85e8d17363a64bdbc4d416708cba2fa
Lukas Kern <lukas.kern2@gmail.com>
2023-04-09 15:23:38 +0200

    Add more tests

--- src/test/java/com/guide/RomanNumeralTest.java
+++ src/test/java/com/guide/RomanNumeralTest.java
@@ -2,9 +2,21 @@ package com.guide;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.matchesPattern;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RomanNumeralTest {
+    @Test
+    void addNumeralsExample1() {
+        assertEquals(RomanNumerals.toInt("MMVI"), 2006);
+    }
+
+    @Test
+    void addNumeralsExample2() {
+        assertEquals(RomanNumerals.toInt("MCMXLIV"), 1944);
+    }
+
     @Test
     void addValuesWhenSmallSucceedsLarge() {
         assertEquals(RomanNumerals.toInt("VI"), 6);
@@ -14,4 +26,56 @@ public class RomanNumeralTest {
     void subtractValuesWhenSmallPrecedesLarge() {
         assertEquals(RomanNumerals.toInt("IV"), 4);
     }
+
+    @Test
+    void dontRepeatIMoreThanThrice() {
+        assertThrows(NumberFormatException.class, () -> {
+            RomanNumerals.toInt("IIII");
+        });
+    }
+
+    @Test
+    void dontRepeatXMoreThanThrice() {
+        assertThrows(NumberFormatException.class, () -> {
+            RomanNumerals.toInt("XXXX");
+        });
+    }
+
+    @Test
+    void dontRepeatCMoreThanThrice() {
+        assertThrows(NumberFormatException.class, () -> {
+            RomanNumerals.toInt("CCCC");
+        });
+    }
+
+    @Test
+    void dontRepeatMMoreThanThrice() {
+        assertThrows(NumberFormatException.class, () -> {
+            RomanNumerals.toInt("MMMM");
+        });
+    }
+
+    @Test
+    void dontRepeatD() {
+        assertThrows(NumberFormatException.class, () -> {
+            RomanNumerals.toInt("DD");
+        });
+    }
+
+    @Test
+    void dontRepeatL() {
+        assertThrows(NumberFormatException.class, () -> {
+            RomanNumerals.toInt("LL");
+        });
+    }
+
+    @Test
+    void dontRepeatV() {
+        assertThrows(NumberFormatException.class, () -> {
+            RomanNumerals.toInt("VV");
+        });
+    }
+
+    
+
 }
