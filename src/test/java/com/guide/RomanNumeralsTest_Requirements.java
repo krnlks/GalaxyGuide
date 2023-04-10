05da6f82a83b595871838a379bb6ed5dc565473a
Lukas Kern <lukas.kern2@gmail.com>
2023-04-10 17:57:17 +0200

    Organize tests in two categories
    One for testing requirements / constraints, and one for testing methods

--- src/test/java/com/guide/RomanNumeralsTest_Requirements.java
+++ src/test/java/com/guide/RomanNumeralsTest_Requirements.java
@@ -2,15 +2,10 @@ package com.guide;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import static com.guide.RomanNumerals.getParts;
-import static com.guide.RomanNumerals.getRomanNumber;
-import static org.junit.jupiter.api.Assertions.*;
-
-public class RomanNumeralTest {
+public class RomanNumeralsTest_Requirements {
     @Test
     void addNumeralsExamples() {
         assertEquals(2006, RomanNumerals.getInt("MMVI"));
@@ -147,61 +142,5 @@ public class RomanNumeralTest {
         assertThrows(NumberFormatException.class, () -> {
             RomanNumerals.getInt("CCM");
         });
-    }
-
-    @Test
-    public void testExtractSummands() {
-        String[] testLiterals = new String[]{
-                "MCMXLIV"
-        };
-
-        List<char[]> summands = RomanNumerals.extractSummands(testLiterals[0]);
-
-        List<char[]> shouldBe = new ArrayList<>();
-        shouldBe.add("M".toCharArray());
-        shouldBe.add("CM".toCharArray());
-        shouldBe.add("XL".toCharArray());
-        shouldBe.add("IV".toCharArray());
-
-        assertArrayEquals(shouldBe.toArray(), summands.toArray());
-    }
-
-    @Test
-    void testGetRomanNumber_int() {
-        assertEquals("MCMIII", getRomanNumber(1903));
-        assertEquals("MMVI", getRomanNumber(2006));
-        assertEquals("MCMXLIV", getRomanNumber(1944));
-        assertEquals("MMMCMX", getRomanNumber(3910));
-        assertEquals("MMMCMXCIX", getRomanNumber(3999));
-    }
-
-    @Test
-    void testGetParts() {
-        var ex1 = new Integer[] {1000, 900, 3};
-        var ex2 = new Integer[] {2000, 6};
-        var ex3 = new Integer[] {1000, 900, 40, 4};
-        var ex4 = new Integer[] {3000, 900, 10};
-        var ex5 = new Integer[] {3000, 900, 90, 9};
-
-        assertArrayEquals(ex1, getParts(1903).toArray());
-        assertArrayEquals(ex2, getParts(2006).toArray());
-        assertArrayEquals(ex3, getParts(1944).toArray());
-        assertArrayEquals(ex4, getParts(3910).toArray());
-        assertArrayEquals(ex5, getParts(3999).toArray());
-    }
-
-    @Test
-    void testGetRomanNumber_parts() {
-        var ex1 = Arrays.asList(1000, 900, 3);
-        var ex2 = Arrays.asList(2000, 6);
-        var ex3 = Arrays.asList(1000, 900, 40, 4);
-        var ex4 = Arrays.asList(3000, 900, 10);
-        var ex5 = Arrays.asList(3000, 900, 90, 9);
-
-        assertEquals("MCMIII", RomanNumerals.getRomanNumber(ex1));
-        assertEquals("MMVI", RomanNumerals.getRomanNumber(ex2));
-        assertEquals("MCMXLIV", RomanNumerals.getRomanNumber(ex3));
-        assertEquals("MMMCMX", RomanNumerals.getRomanNumber(ex4));
-        assertEquals("MMMCMXCIX", RomanNumerals.getRomanNumber(ex5));
     }
 }
