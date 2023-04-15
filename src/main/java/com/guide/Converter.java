dce2b5b7c9c8fc044288f293d1ea30729df86255
Lukas Kern <lukas.kern2@gmail.com>
2023-04-15 19:12:13 +0200

    Add basic chat functionality

--- src/main/java/com/guide/Converter.java
+++ src/main/java/com/guide/Converter.java
@@ -1,6 +1,38 @@
 package com.guide;
+
+import java.util.Scanner;
+
 public class Converter {
-    public static String input(String input){
+    public static String input(String input) {
+        input = input.toLowerCase().trim();
+
+        if (input.contains("how much is")) {
+            return "It's that much!";
+        } else if (input.contains("how many credits is")) {
+            return "That's so and so many credits!";
+        }
+
         return "";
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+
+        System.out.println("Welcome to the Intergalactic Numbers and Units Converter! Type 'exit' to quit.");
+
+        while (!exit) {
+            String input = scanner.nextLine();
+
+            if ("exit".equalsIgnoreCase(input)) {
+                exit = true;
+            } else {
+                String response = input(input);
+                if (response.length() > 0)
+                    System.out.println("Response: " + response);
+            }
+        }
+
+        System.out.println("Goodbye!");
     }
 }
