bf7273b6cc6dd88bd19d94ba198f225c7767bffe
Lukas Kern <lukas.kern2@gmail.com>
2023-04-16 00:43:54 +0200

    Write condition for number conversion query

--- src/main/java/com/guide/Converter.java
+++ src/main/java/com/guide/Converter.java
@@ -16,13 +16,21 @@ public class Converter {
         input = input.toLowerCase().trim();
 
         String[] arr = input.split(" ");
-        boolean isDefinition = (arr.length == 3 && arr [1] == "is"
+        boolean isDefinition = (arr.length == 3
+                && arr [1] == "is"
                 && Arrays.asList("i","v","x","l","c","d","m").contains(arr[2]));
-        if (isDefinition){
+        if (isDefinition) {
             termsToNumerals.put(arr[0], arr[2]);
-        } else if (input.contains("how much is")) {
+            return "";
+        }
+
+        boolean isNumberConversionQuery = (arr.length > 4
+                && input.substring(0,11).equals("how much is")
+                && arr[arr.length-1] == "?");
+        if (isNumberConversionQuery){
             return "It's that much!";
-        } else if (input.contains("how many credits is")) {
+        }
+        if (input.contains("how many credits is")) {
             return "That's so and so many credits!";
         }
 
