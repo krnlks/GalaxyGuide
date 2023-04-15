731a1923e9631d60def62d32bd294f5844cdef6f
Lukas Kern <lukas.kern2@gmail.com>
2023-04-16 00:03:37 +0200

    Add functionality to assign Roman numerals to strings

--- src/main/java/com/guide/Converter.java
+++ src/main/java/com/guide/Converter.java
@@ -1,12 +1,26 @@
 package com.guide;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Converter {
-    public static String input(String input) {
+    Map<String,String> termsToNumerals;
+
+    public Converter(){
+        termsToNumerals = new HashMap<>(7);
+    }
+
+    public String input(String input) {
         input = input.toLowerCase().trim();
 
-        if (input.contains("how much is")) {
+        String[] arr = input.split(" ");
+        boolean isDefinition = (arr.length == 3 && arr [1] == "is"
+                && Arrays.asList("I","V","X","L","C","D","M").contains(arr[2]));
+        if (isDefinition){
+            termsToNumerals.put(arr[0], arr[2]);
+        } else if (input.contains("how much is")) {
             return "It's that much!";
         } else if (input.contains("how many credits is")) {
             return "That's so and so many credits!";
@@ -16,6 +30,8 @@ public class Converter {
     }
 
     public static void main(String[] args) {
+        Converter c = new Converter();
+
         Scanner scanner = new Scanner(System.in);
         boolean exit = false;
 
@@ -27,7 +43,7 @@ public class Converter {
             if ("exit".equalsIgnoreCase(input)) {
                 exit = true;
             } else {
-                String response = input(input);
+                String response = c.input(input);
                 if (response.length() > 0)
                     System.out.println("Response: " + response);
             }
